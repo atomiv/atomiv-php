00cfa1130c7a4a462ed14d9903b3f38c0f580e7d
darko96rakic <rakicdarko96@gmail.com>
2022-01-28 14:29:28 +0100

    feat: added getters and setters

--- app/Entities/Customer.php
+++ app/Entities/Customer.php
@@ -27,8 +27,23 @@ class Customer
      */
     private $lastName;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setFirstName(string $firstName){
+        $this->firstName = $firstName;
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function setLastName(string $lastName){
+        $this->lastName = $lastName;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
 }
