97fbc735e8d8a7c6c7c50c12ca4b4dd98c8ffca7
darko96rakic <rakicdarko96@gmail.com>
2022-09-23 12:22:17 +0200

    Added Product response dto

---
+++ app/Services/Dto/CreateProductResponseDto.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Services\Dto;
+
+
+class CreateProductResponseDto
+{
+    private $id;
+    private $code;
+    private $description;
+    private $unit_price;
+
+    public function setId(int $id){
+        $this->id = $id;
+    }
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code)
+    {
+        return $this->code = $code;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description)
+    {
+       return $this->description = $description;
+    }
+
+    public function getUnitPrice(){
+        return $this->unit_price;
+    }
+
+    public function setUnitPrice(float $unit_price){
+        return $this->unit_price = $unit_price;
+    }
+}
