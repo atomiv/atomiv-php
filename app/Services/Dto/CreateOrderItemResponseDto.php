02df0b19e046e6d2895e3dce51c3e6caa72fdefa
darko96rakic <rakicdarko96@gmail.com>
2022-09-23 18:06:34 +0200

    Added update and create response dto

---
+++ app/Services/Dto/CreateOrderItemResponseDto.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Services\Dto;
+
+
+class CreateOrderItemResponseDto
+{
+    private $id;
+    private $product_id;
+    private $quantity;
+
+    public function setId(int $id){
+        $this->id = $id;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getProductId(){
+        return $this->product_id;
+    }
+
+    public function setProductId(int $productId){
+        return $this->product_id = $productId;
+    }
+
+    public function getQuantity(){
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity){
+        return$this->quantity = $quantity;
+    }
+}
---
+++ app/Services/Dto/CreateOrderResponseDto.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Services\Dto;
+
+class CreateOrderResponseDto
+{
+    private $id;
+    private $customer_id;
+    private $order_date;
+    private $order_items = [];
+
+    public function setId(int $id){
+        $this->id = $id;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getCustomerId(){
+        return $this->customer_id;
+    }
+
+    public function setCustomerId($customer_id)
+    {
+        return $this->customer_id = $customer_id;
+    }
+
+    public function getOrderDate(){
+        return $this->order_date;
+    }
+
+    public function setOrderDate($order_date){
+        return $this->order_date = $order_date;
+    }
+
+    public function getOrderItems(){
+        return $this->order_items;
+    }
+
+    public function setOrderItems($orderItem)
+    {
+        array_push($this->order_items,$orderItem);
+    }
+}
