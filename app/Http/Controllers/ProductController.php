4bf3a4c6e8bb1cecb4d27a3e2f465087728bad7c
darko96rakic <rakicdarko96@gmail.com>
2022-12-23 11:56:45 +0100

    Handlers used in the contoller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,21 +7,39 @@ use App\Http\Requests\Products\CreateProductFormRequest;
 use App\Http\Requests\Products\UpdateProductFormRequest;
 use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
-use App\Services\Dto\CreateProductRequestDto;
-use App\Services\Dto\UpdateProductRequestDto;
-use App\Services\ProductService;
+use App\Services\BrowseProducts\GetAllProductsHandler;
+use App\Services\CreateProduct\CreateProductHandler;
+use App\Services\CreateProduct\CreateProductRequest;
+use App\Services\GetProduct\GetProductHandler;
+use App\Services\RemoveProduct\RemoveProductHandler;
+use App\Services\UpdateProduct\UpdateProductHandler;
+use App\Services\UpdateProduct\UpdateProductRequest;
 
 class ProductController extends Controller
 {
-    private $productService;
+    private $updateProductHandler;
+    private $createProductHandler;
+    private $getProductHandler;
+    private $getAllProductsHandler;
+    private $removeProductHandler;
 
-    public function __construct(ProductService $productService){
-        $this->productService = $productService;
+    public function __construct(
+        UpdateProductHandler $updateProductHandler,
+        CreateProductHandler $createProductHandler,
+        GetProductHandler $getProductHandler,
+        GetAllProductsHandler $getAllProductsHandler,
+        RemoveProductHandler $removeProductHandler
+    ){
+        $this->createProductHandler = $createProductHandler;
+        $this->updateProductHandler = $updateProductHandler;
+        $this->removeProductHandler = $removeProductHandler;
+        $this->getProductHandler = $getProductHandler;
+        $this->getAllProductsHandler = $getAllProductsHandler;
     }
 
     public function getProduct($id){
 
-        $product = $this->productService->getProduct($id);
+        $product = $this->getProductHandler->handle($id);
 
         if ($product)
             return response(new ProductResource($product),200);
@@ -31,37 +49,37 @@ class ProductController extends Controller
 
     public function getAllProducts(){
 
-        $products = $this->productService->getAllProducts();
+        $products = $this->getAllProductsHandler->handle();
 
         return response(new ProductCollection($products),200);
     }
 
     public function create(CreateProductFormRequest $request){
 
-        $requestDto = new CreateProductRequestDto();
+        $requestDto = new CreateProductRequest();
         $requestDto->setCode($request->code);
         $requestDto->setDescription($request->description);
         $requestDto->setUnitPrice($request->unit_price);
 
-        $product = $this->productService->add($requestDto);
+        $product = $this->createProductHandler->handle($requestDto);
 
         return response(new ProductResource($product),201);
     }
 
     public function update(UpdateProductFormRequest $request, $id){
 
-        $requestDto = new UpdateProductRequestDto();
+        $requestDto = new UpdateProductRequest();
         $requestDto->setCode($request->code);
         $requestDto->setDescription($request->description);
         $requestDto->setUnitPrice($request->unit_price);
 
-        $product = $this->productService->update($requestDto,$id);
+        $product = $this->updateProductHandler->handle($requestDto,$id);
 
         return response([new ProductResource($product),'message' =>'Product successfully updated'],200);
     }
 
     public function delete($id){
-        $this->productService->remove($id);
+        $this->removeProductHandler->handle($id);
 
         return response('Product successfully deleted',200);
     }
